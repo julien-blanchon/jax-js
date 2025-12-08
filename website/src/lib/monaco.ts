00498fc50723fa30b9a08b4149338ce66c41e243
Eric Zhang <ekzhang1@gmail.com>
2025-12-08 11:00:39 -0800

    Remove unused languages from monaco

--- website/src/lib/monaco.ts
+++ website/src/lib/monaco.ts
@@ -3,9 +3,9 @@ import loadersTypesSrc from "@jax-js/loaders/dist/index.d.ts?raw";
 import optaxTypesSrc from "@jax-js/optax/dist/index.d.ts?raw";
 import * as monaco from "monaco-editor";
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
-import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+// import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
+// import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
+// import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
 import replBuiltinsSrc from "../routes/repl/repl-builtins.d.ts?raw";
@@ -13,16 +13,16 @@ import replBuiltinsSrc from "../routes/repl/repl-builtins.d.ts?raw";
 self.MonacoEnvironment = {
   getWorker: function (_: string, label: string) {
     switch (label) {
-      case "json":
-        return new jsonWorker();
-      case "css":
-      case "scss":
-      case "less":
-        return new cssWorker();
-      case "html":
-      case "handlebars":
-      case "razor":
-        return new htmlWorker();
+      // case "json":
+      //   return new jsonWorker();
+      // case "css":
+      // case "scss":
+      // case "less":
+      //   return new cssWorker();
+      // case "html":
+      // case "handlebars":
+      // case "razor":
+      //   return new htmlWorker();
       case "typescript":
       case "javascript":
         return new tsWorker();
